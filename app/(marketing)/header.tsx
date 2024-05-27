51895ba8f929a5382b71bda6c885086d048b0cd1
anh.nt <anh.nt@vn-cubesystem.com>
2024-05-27 18:11:33 +0700

    feat:sidebar layout userprogress

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,6 +1,14 @@
-import { ClerkLoading } from "@clerk/nextjs";
-import { Loader } from "lucide-react";
 import Image from "next/image";
+import { Loader } from "lucide-react";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
 
 const Header = () => {
   return (
@@ -9,12 +17,28 @@ const Header = () => {
         <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
           <Image src="/mascot.svg" height={40} width={40} alt="Mascot" />
           <h1 className="text-2xl font-extrabold text-green-800 tracking-wide">
-            owlingo
+            Owlingo
           </h1>
         </div>
         <ClerkLoading>
-          <Loader />
+          <Loader className="w-5 h-5 text-muted-foreground animate-spin" />
         </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton
+              mode="modal"
+              fallbackRedirectUrl="/learn"
+              signUpFallbackRedirectUrl="/learn"
+            >
+              <Button size="lg" variant="ghost">
+                Login
+              </Button>
+            </SignInButton>
+          </SignedOut>
+        </ClerkLoaded>
       </div>
     </header>
   );
