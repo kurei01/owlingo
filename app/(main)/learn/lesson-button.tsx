5d3fbfc24b83f874a5f9eaa3fd5dc76879110bdf
anh.nt <anh.nt@vn-cubesystem.com>
2024-06-10 16:02:03 +0700

    courseProgress

--- app/(main)/learn/lesson-button.tsx
+++ app/(main)/learn/lesson-button.tsx
@@ -66,13 +66,55 @@ export const LessonButton = ({
         }}
       >
         {current ? (
-          <div className="h-[120px] w-[120px] relative">
+          <div className="h-[102px] w-[102px] relative">
             <div className="absolute -top-6 left-2.5 px-3 py-2.5 border-2 font-bold uppercase text-green-500 bg-white rounded-xl animate-bounce tracking-wide z-10">
               Start
+              <div className="absolute left-1/2 transform -translate-x-1/2 -bottom-2 w-0 h-0 border-x-8 border-x-transparent border-t-8" />
             </div>
+            <CircularProgressbarWithChildren
+              value={Number.isNaN(percentage) ? 0 : percentage}
+              styles={{
+                path: {
+                  stroke: "#4ade80",
+                },
+                trail: {
+                  stroke: "#e5e7eb",
+                },
+              }}
+            >
+              <Button
+                size="rounded"
+                variant={locked ? "locked" : "secondary"}
+                className="h-[70px] w-[70px] border-b-8"
+              >
+                <Icon
+                  className={cn(
+                    "h-10 w-10",
+                    locked
+                      ? "fill-neutral-400 text-neutral-400 stroke-neutral-400"
+                      : "fill-primary-foreground text-primary-foreground",
+                    isCompleted && "fill-none stroke-[4]"
+                  )}
+                />
+              </Button>
+            </CircularProgressbarWithChildren>
           </div>
         ) : (
-          <div>Something</div>
+          <Button
+            size="rounded"
+            variant={locked ? "locked" : "secondary"}
+            className="h-[70px] w-[70px] border-b-8"
+          >
+            <Icon
+              className={cn(
+                "h-10 w-10",
+                locked
+                  ? "fill-neutral-400 text-neutral-400 stroke-neutral-400"
+                  : "fill-primary-foreground text-primary-foreground",
+                isCompleted && "fill-none stroke-[4]"
+              )}
+            />
+          </Button>
         )}
       </div>
     </Link>
